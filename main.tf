2d4453bad756010d3435c9d747225f32d28d8df4
StratoStack <ravishankarr2023@gmail.com>
2025-01-04 23:40:22 +0800

    added random id for bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2"  # Ensure this matches your region
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-s3-bucket-12345" # Replace with a unique name
+  bucket = "my-terraform-s3-bucket-${random_id.bucket_suffix.hex}"  # Append unique suffix
   acl    = "private"
 }
